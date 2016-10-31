eaa17a82456b03c01fb26ab4996262e0c1bab798
eran mor <eranmr@yahoo.com>
2016-10-30 20:49:25 -0700

    -a

--- music/views.py
+++ music/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import Album
-
+from django.http import Http404
 
 # Create your views here.
+# render has httprespnse built in
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
 
 '''
     html = ''
@@ -22,6 +21,15 @@ def index(request):
 
 '''
 
-
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
+
+    # This was the response before connecting to the DB
+    # return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
+
+    try:
+        album = Album.objects.get(pk=album_id)
+        context = {'album': album}
+    except Album.DoesNotExist:
+        raise Http404("Sorry, album does not exist")
+
+    return render(request, 'music/detail.html', context)
